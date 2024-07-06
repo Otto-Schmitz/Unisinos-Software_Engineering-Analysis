8fd1979f2bcaa9d68599cd33e4ec7a65d39c1a46
Gabriel <41166074+gckopper@users.noreply.github.com>
2024-07-06 01:41:25 -0300

    fix patron and ticket imports

--- src/main/java/com/se/Repositories/PatronRepository.java
+++ src/main/java/com/se/Repositories/PatronRepository.java
@@ -1,6 +1,6 @@
 package com.se.Repositories;
 
-import com.se.Entities.Patron;
+import com.se.Entities.patron.Patron;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatronRepository extends JpaRepository<Patron, Long> {
--- src/main/java/com/se/Repositories/TicketRepository.java
+++ src/main/java/com/se/Repositories/TicketRepository.java
@@ -1,6 +1,6 @@
 package com.se.Repositories;
 
-import com.se.Entities.Ticket;
+import com.se.Entities.ticket.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
